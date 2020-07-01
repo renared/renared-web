2bed4e0715876649daadb8dd1553726df45aad30
Yann Bourdin <yann.bourdin@gmail.com>
2020-07-01 22:58:52 +0200

    polls.models et migré

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Poll(models.Model):
+    question_text = models.CharField(max_length=300)
+    #author = models.CharField(max_length=42)
+    date = models.DateTimeField(default=timezone.now, verbose_name="Date de parution")
+    choice_type = models.CharField(max_length=12, verbose_name="Type de choix. 'UNIQUE' pour des ptits ronds, 'MULTIPLE' pour des carrés, 'POURCENT' pour des pourcentages")
+
+    class Meta:
+        verbose_name = "Sondage"
+        ordering =  ["date"]
+
+class Choice(models.Model):
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    votes = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = "Choix"
+
+class VoteHash(models.Model):
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE)
+    hash = models.SlugField(max_length=32)
+
+    class Meta:
+        verbose_name = "Choix"
+        ordering = ["poll"]
--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path('', views.index),
-    path('<id_poll>', views.detail),
-    path('<id_poll>/vote', views.vote),
-    path('<id_poll>/results', views.results),
+    path('', views.index, name="index"),
+    path('<id_poll>', views.detail, name="detail"),
+    path('<id_poll>/vote', views.vote, name="vote"),
+    path('<id_poll>/results', views.results, name="results"),
 ]
